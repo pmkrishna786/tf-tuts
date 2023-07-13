b254aaead2058e6208676932d86660e1eba0abcc
pmkrishna786 <80220383+pmkrishna786@users.noreply.github.com>
2023-07-13 23:26:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  access_key = "AKIA3DSBZQFHJJSSOHYD"
+  secret_key = "REP56pl03R2KPpiJTpoKjVYvHBcZbdV/RJkvrxrn"
+  region     = "us-east-1"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-023b120e01f4779c1"
   instance_type = "t2.micro"
